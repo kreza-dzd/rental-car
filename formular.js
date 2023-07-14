5f406f5b44de38108497bb2984a597a36bb151a5
Marko Krezic <markokrezic90@gmail.com>
2023-07-14 22:18:55 +0200

    Update formular.js

--- formular.js
+++ formular.js
@@ -1,24 +1,61 @@
 const navSlide = () => {
-    const menuIcon = document.querySelector('#menu-icon');
-    const nav = document.querySelector('.navbar');
-    const navbar = document.querySelectorAll('.navbar li');
- 
-    menuIcon.addEventListener('click', () => {
-       nav.classList.toggle('nav-active');
- 
-       navbar.forEach((link, index) => {
- 
-          if (link.style.animation) {
-             link.style.animation = '';
-          } else {
-             link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 0.3}s`;
-          }
-       });
-    });
- 
-    
- }
+   const menuIcon = document.querySelector('#menu-icon');
+   const nav = document.querySelector('.navbar');
+   const navbar = document.querySelectorAll('.navbar li');
 
- navSlide();
+   menuIcon.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+
+      navbar.forEach((link, index) => {
+
+         if (link.style.animation) {
+            link.style.animation = '';
+         } else {
+            link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 0.3}s`;
+         }
+      });
+   });
+
+   
+}
+
+navSlide();
+
+
+document.addEventListener("DOMContentLoaded", function() {
+   var defaultCheckinDate = new Date();
+   defaultCheckinDate.setHours(9);
+   defaultCheckinDate.setMinutes(0);
+   defaultCheckinDate.setSeconds(0);
+
+   var defaultCheckoutDate = new Date();
+   defaultCheckoutDate.setHours(8);
+   defaultCheckoutDate.setMinutes(0);
+   defaultCheckoutDate.setSeconds(0);
+
+   flatpickr("#checkin", {
+       enableTime: true,
+       dateFormat: "Y-m-d H:i",
+       defaultDate: defaultCheckinDate,
+       time_24hr: true,
+   });
+   flatpickr("#checkout", {
+       enableTime: true,
+       dateFormat: "Y-m-d H:i",
+       defaultDate: defaultCheckoutDate,
+       time_24hr: true,
+   });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
